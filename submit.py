1daad49f2a4b9f6e96dceea61dc484dd59acf4e8
Your Name <you@example.com>
2022-06-13 11:43:17 +0800

    2022.06.03 11:43 | 更新了代码提交模块

--- submit.py
+++ submit.py
@@ -8,7 +8,7 @@ a = input("Description: >>")
 locale.setlocale(locale.LC_CTYPE, 'Chinese')
 
 debug = False
-date_time = datetime.datetime.now().strftime("%Y.%m.%d | %H:%M | "+a)
+date_time = datetime.datetime.now().strftime("%Y.%m.%d %H:%M | "+a)
 
 if not debug:
     system("git add .")
