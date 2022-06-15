d748b466859b1f8bdb1662f7ff700dbbe17f7a59
WhiteZh <whitewolfzhang@gmail.com>
2022-06-15 14:50:39 +0800

    2022.06.15 14:50 |

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,17 +4,19 @@ package frc.robot;
 import frc.robot.subsystems.DriveSystem; 
 import frc.robot.subsystems.LiftSystem;
 import frc.robot.subsystems.PneumaticSystem;
-import frc.robot.subsystems.VisionSystem;
+import frc.robot.subsystems.TurrentSystem;
+//import frc.robot.subsystems.VisionSystem;
 import frc.robot.subsystems.intakeSystem;
-import frc.robot.subsystems.upAndShootSystem;
+//import frc.robot.subsystems.upAndShootSystem;
 
 import frc.robot.commands.ArcadeDrive;
 import frc.robot.commands.I_Will;
 import frc.robot.commands.Intakecomm;
 import frc.robot.commands.LiftComm;
 import frc.robot.commands.PCM_Conctrl;
-import frc.robot.commands.UpAndShootComm;
-import frc.robot.commands.VisionCmd;
+import frc.robot.commands.TurrentCmd;
+//import frc.robot.commands.UpAndShootComm;
+//import frc.robot.commands.VisionCmd;
 import frc.robot.joysticks.DriveStick;
 import frc.robot.joysticks.OperateStick;
 import edu.wpi.first.wpilibj2.command.Command;
@@ -22,11 +24,12 @@ import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 
 public class RobotContainer {
     private final DriveSystem m_driveSubsystem = new DriveSystem();
-    private final upAndShootSystem m_upAndShootSystem = new upAndShootSystem();
+    //private final upAndShootSystem m_upAndShootSystem = new upAndShootSystem();
     private final PneumaticSystem m_PneumaticSystem = new PneumaticSystem(); // 气动系统
     private final intakeSystem m_IntakeSystem = new intakeSystem();
     private final LiftSystem m_LiftSystem = new LiftSystem();
-    private final VisionSystem m_VisionSystem = new VisionSystem();
+    //private final VisionSystem m_VisionSystem = new VisionSystem();
+    private final TurrentSystem m_TurrentSystem = new TurrentSystem();
 
     private final DriveStick joystick1 = new DriveStick();
     private final OperateStick joystick2 = new OperateStick();
@@ -47,12 +50,12 @@ public class RobotContainer {
             () -> joystick2.getIntakeStatus()
 		));
 
-        m_upAndShootSystem.setDefaultCommand(new UpAndShootComm(
+        /*m_upAndShootSystem.setDefaultCommand(new UpAndShootComm(
 			m_upAndShootSystem,
                 
 			() -> joystick2.shallShootBallForward(),
             () -> joystick2.shallMoveBallUp()
-		));
+		));*/
 
         m_PneumaticSystem.setDefaultCommand(new PCM_Conctrl(
 			m_PneumaticSystem,
@@ -68,13 +71,21 @@ public class RobotContainer {
             () -> joystick1.getRawButton(2)
 		));
 
-        m_VisionSystem.setDefaultCommand(new VisionCmd(
+        /*m_VisionSystem.setDefaultCommand(new VisionCmd(
             m_upAndShootSystem,
             m_VisionSystem,
 
             () -> joystick2.getHorizontal(),
             () -> joystick2.isFire()       
-        ));
+        ));*/
+
+        m_TurrentSystem.setDefaultCommand(new TurrentCmd(
+            m_TurrentSystem, 
+            
+            () -> joystick2.shallShootBallForward(), 
+            () -> joystick2.shallMoveBallUp(), 
+            () -> joystick2.getHorizontal(), 
+            () -> joystick2.isFire()));
     }
 
     public Command getAutonomousCommand() {
@@ -87,8 +98,7 @@ public class RobotContainer {
 			m_IntakeSystem.getDefaultCommand(),
 			m_LiftSystem.getDefaultCommand(),
 			m_PneumaticSystem.getDefaultCommand(),
-			m_VisionSystem.getDefaultCommand(),
-			m_upAndShootSystem.getDefaultCommand()
+			m_TurrentSystem.getDefaultCommand()
 		);
 	}
 
