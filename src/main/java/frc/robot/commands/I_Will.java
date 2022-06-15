2f941839341bf043cc3f83629d7d6dc503839cc9
WhiteZh <whitewolfzhang@gmail.com>
2022-06-15 09:51:49 +0800

    2022.06.15 09:51 |

--- src/main/java/frc/robot/commands/I_Will.java
+++ src/main/java/frc/robot/commands/I_Will.java
@@ -1,3 +1,13 @@
+/*
+ * The name of the class is chosen from a song with the same name.
+ * It is a good song and if anyone see this, you may go take a try.
+ * What I hear is not the original one but a guitar cover version.
+ * Platform is NetEase Music, written by DongNi Fingerstyle.
+ * Hope you enjoy.
+ */
+
+
+
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj.Timer;
