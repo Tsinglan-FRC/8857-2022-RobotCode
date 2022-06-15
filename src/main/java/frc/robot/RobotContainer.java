d44577f64bf951507319944f0f8cf81a147335b3
WhiteZh <whitewolfzhang@gmail.com>
2022-06-15 13:36:37 +0800

    2022.06.15 13:36 |

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -21,75 +21,75 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 
 public class RobotContainer {
-        private final DriveSystem m_driveSubsystem = new DriveSystem();
-        private final upAndShootSystem m_upAndShootSystem = new upAndShootSystem();
-        private final PneumaticSystem m_PneumaticSystem = new PneumaticSystem(); // 气动系统
-        private final intakeSystem m_IntakeSystem = new intakeSystem();
-        private final LiftSystem m_LiftSystem = new LiftSystem();
-        private final VisionSystem m_VisionSystem = new VisionSystem();
+    private final DriveSystem m_driveSubsystem = new DriveSystem();
+    private final upAndShootSystem m_upAndShootSystem = new upAndShootSystem();
+    private final PneumaticSystem m_PneumaticSystem = new PneumaticSystem(); // 气动系统
+    private final intakeSystem m_IntakeSystem = new intakeSystem();
+    private final LiftSystem m_LiftSystem = new LiftSystem();
+    private final VisionSystem m_VisionSystem = new VisionSystem();
 
-        private final DriveStick joystick1 = new DriveStick();
-        private final OperateStick joystick2 = new OperateStick();
+    private final DriveStick joystick1 = new DriveStick();
+    private final OperateStick joystick2 = new OperateStick();
 
-        public RobotContainer() {
-            m_driveSubsystem.setDefaultCommand(new ArcadeDrive(
-				m_driveSubsystem, // 定义手柄
+    public RobotContainer() {
+        m_driveSubsystem.setDefaultCommand(new ArcadeDrive(
+			m_driveSubsystem, // 定义手柄
 	            
-				() -> joystick1.getSpeed(),
-				() -> joystick1.getTurn(),
-				() -> joystick1.isInLowSpeed()
-			));
+			() -> joystick1.getSpeed(),
+			() -> joystick1.getTurn(),
+			() -> joystick1.isInLowSpeed()
+		));
 
-            m_IntakeSystem.setDefaultCommand(new Intakecomm(
-                m_IntakeSystem,
+        m_IntakeSystem.setDefaultCommand(new Intakecomm(
+            m_IntakeSystem,
                         
-            	() -> joystick2.shallPutOut(),
-                () -> joystick2.getIntakeStatus()
-			));
+        	() -> joystick2.shallPutOut(),
+            () -> joystick2.getIntakeStatus()
+		));
 
-            m_upAndShootSystem.setDefaultCommand(new UpAndShootComm(
-				m_upAndShootSystem,
+        m_upAndShootSystem.setDefaultCommand(new UpAndShootComm(
+			m_upAndShootSystem,
                 
-				() -> joystick2.shallShootBallForward(),
-                () -> joystick2.shallMoveBallUp()
-			));
+			() -> joystick2.shallShootBallForward(),
+            () -> joystick2.shallMoveBallUp()
+		));
 
-            m_PneumaticSystem.setDefaultCommand(new PCM_Conctrl(
-				m_PneumaticSystem,
+        m_PneumaticSystem.setDefaultCommand(new PCM_Conctrl(
+			m_PneumaticSystem,
 	            
-				() -> joystick1.getCompressorStatus()
-			));
+			() -> joystick1.getCompressorStatus()
+		));
 
-            m_LiftSystem.setDefaultCommand(new LiftComm(
-				m_LiftSystem,
+        m_LiftSystem.setDefaultCommand(new LiftComm(
+			m_LiftSystem,
                 
-				() -> joystick1.getRawAxis(2),
-                () -> joystick1.getRawAxis(3),
-                () -> joystick1.getRawButton(2)
-			));
+			() -> joystick1.getRawAxis(2),
+            () -> joystick1.getRawAxis(3),
+            () -> joystick1.getRawButton(2)
+		));
 
-            m_VisionSystem.setDefaultCommand(new VisionCmd(
-                m_upAndShootSystem,
-                m_VisionSystem,
+        m_VisionSystem.setDefaultCommand(new VisionCmd(
+            m_upAndShootSystem,
+            m_VisionSystem,
 
-                () -> joystick2.getHorizontal(),
-                () -> joystick2.isFire()       
-            ));
-        }
+            () -> joystick2.getHorizontal(),
+            () -> joystick2.isFire()       
+        ));
+    }
 
-        public Command getAutonomousCommand() {
-            return new I_Will(m_driveSubsystem);
-        }
+    public Command getAutonomousCommand() {
+        return new I_Will(m_driveSubsystem);
+    }
 
-		public Command getTeleopCommand(){
-			return new SequentialCommandGroup(
-				m_driveSubsystem.getDefaultCommand(),
-				m_IntakeSystem.getDefaultCommand(),
-				m_LiftSystem.getDefaultCommand(),
-				m_PneumaticSystem.getDefaultCommand(),
-				m_VisionSystem.getDefaultCommand(),
-				m_upAndShootSystem.getDefaultCommand()
-			);
-		}
+	public Command getTeleopCommand(){
+		return new SequentialCommandGroup(
+			m_driveSubsystem.getDefaultCommand(),
+			m_IntakeSystem.getDefaultCommand(),
+			m_LiftSystem.getDefaultCommand(),
+			m_PneumaticSystem.getDefaultCommand(),
+			m_VisionSystem.getDefaultCommand(),
+			m_upAndShootSystem.getDefaultCommand()
+		);
+	}
 
 }
